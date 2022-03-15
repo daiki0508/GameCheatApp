79f68847826d00a44984fe1bea27d4d0ad6242c4
daiki0508 <oochani05085@outlook.jp>
2022-03-15 22:15:31 +0900

    refactor: Change package configuration.

--- additional_implementation/GameCheatPinning/app/src/main/java/com/websarva/wings/android/gamecheatpinning/ui/MainActivity.kt
+++ additional_implementation/GameCheatPinning/app/src/main/java/com/websarva/wings/android/gamecheatpinning/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.websarva.wings.android.gamecheatpinning
+package com.websarva.wings.android.gamecheatpinning.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.websarva.wings.android.gamecheatpinning.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
